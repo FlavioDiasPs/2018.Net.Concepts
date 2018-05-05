5de6b02427e67b074c26d4e6e5cb666fc51bff8b
Flávio Silva <flaviodiasps@hotmail.com>
2018-05-05 19:18:39 -0300

    automapper

---
+++ AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        //public AutomapperWebProfile()
+        //{
+
+        //    //CreateMap<EmployeeDomainModel, EmployeeViewModel>();
+
+        //    //CreateMap<EmployeeViewModel, EmployeeDomainModel>();
+
+        //}
+    }
+}
---
+++ AutoMapper/Model/EmployeeDomainModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMapper.Model
+{
+    public class EmployeeDomainModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+}
