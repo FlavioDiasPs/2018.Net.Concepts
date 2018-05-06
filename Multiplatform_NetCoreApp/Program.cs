1a8fef7fc54e05a38b099699ad7946a813be80cd
Flávio Silva <flaviodiasps@hotmail.com>
2018-05-06 15:23:58 -0300

    g

--- Multiplatform_NetCoreApp/Program.cs
+++ Multiplatform_NetCoreApp/Program.cs
@@ -6,7 +6,7 @@ namespace Multiplatform_NetCoreApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Alteração no csproj");
         }
     }
 }
