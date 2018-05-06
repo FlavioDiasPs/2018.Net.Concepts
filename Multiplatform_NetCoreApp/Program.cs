eca00e236cecee99622bfa4fb0157727c2d77a89
Flávio Silva <flaviodiasps@hotmail.com>
2018-05-06 15:19:40 -0300

    Permission problem

---
+++ Multiplatform_NetCoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiplatform_NetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
